c68558ee09a4db97d390a9c1588eb37ddc9f8c16
Mark McGranaghan <mmcgrana@gmail.com>
2009-01-12 23:20:50 -0500

    Clean up for announce

--- src/ring/examples/hello_world.clj
+++ src/ring/examples/hello_world.clj
@@ -1,8 +1,8 @@
 ; A very simple Ring application.
 
-(ns ring.examples.hello_world
+(ns ring.examples.hello-world
   (:require ring.jetty)
-  (:import java.util.Date (java.text DateFormat SimpleDateFormat)))
+  (:import java.util.Date java.text.SimpleDateFormat))
 
 (def formatter (SimpleDateFormat. "HH:mm:ss"));
 
@@ -11,6 +11,7 @@
   {:status  200
    :headers {"Content-Type" "text/html"}
    :body    (str "<h3>Hello World from Ring</h3>"
-                 "<p>The current time is " (.format formatter (Date.)) ".</p>")})
+                 "<p>The current time is " 
+                 (.format formatter (Date.)) ".</p>")})
 
 (ring.jetty/run {:port 8080} app)
