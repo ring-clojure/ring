db24585e77add8507f84c9497cb62bae940c65ee
Mark McGranaghan <mmcgrana@gmail.com>
2009-07-12 10:53:49 -0400

    Backtrace -> Stacktrace.

--- src/ring/middleware/stacktrace.clj
+++ src/ring/middleware/stacktrace.clj
@@ -29,7 +29,7 @@
          [:html {:xmlns "http://www.w3.org/1999/xhtml"}
            [:head
              [:meta {:http-equiv "Content-Type" :content "text/html;charset=utf-8"}]
-             [:title "Ring: Backtrace"]
+             [:title "Ring: Stacktrace"]
              [:style {:type "text/css"} css]
              [:body
                [:div#content
