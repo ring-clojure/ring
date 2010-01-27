5e7610920ee7df4e490eb9ae63bd26cf2593a146
Mark McGranaghan <mmcgrana@gmail.com>
2010-01-26 20:45:57 -0500

    Depend on clojure-contrib 1.1.0 final.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "A Clojure web applications library."
   :url "http://github.com/mmcgrana/ring"
   :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.1.0"]
                  [org.mortbay.jetty/jetty "6.1.14"]
                  [org.mortbay.jetty/jetty-util "6.1.14"]
                  [org.mortbay.jetty/servlet-api-2.5 "6.1.14"]
@@ -10,5 +10,6 @@
                  [org.apache.httpcomponents/httpcore-nio "4.0.1"]
                  [clj-html "0.1.0-SNAPSHOT"]
                  [clj-stacktrace "0.1.0-SNAPSHOT"]]
-  :repositories [["mvnrepository" "http://mvnrepository.com/"]]
+  :repositories [["mvnrepository" "http://mvnrepository.com/"]
+                 ["clojure-releases" "http://build.clojure.org/releases"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
