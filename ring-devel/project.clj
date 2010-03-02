6b1dece19e2546318b7b0f75e7f629384d1b76e0
Mark McGranaghan <mmcgrana@gmail.com>
2010-03-02 16:25:28 -0500

    Stable clj-stacktrace.

--- ring-devel/project.clj
+++ ring-devel/project.clj
@@ -3,5 +3,5 @@
   :url "http://github.com/mmcgrana/ring"
   :dependencies [[ring/ring-core "0.2.0-SNAPSHOT"]
                  [clj-html "0.1.0-SNAPSHOT"]
-                 [clj-stacktrace "0.1.0-SNAPSHOT"]]
+                 [clj-stacktrace "0.1.0"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
