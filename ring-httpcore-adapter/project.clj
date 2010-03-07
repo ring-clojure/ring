b9137d77164d7382df0da6bcefaabb9c65eaea6d
weavejester <jreeves@weavejester.com>
2010-03-07 00:05:39 +0000

    Revert "Added ':namespaces :all' to project files to enable AOT compilation on Lein 1.1.0"
    This reverts commit 899de93c3a9b572953795135f25be78cf48e312f.

--- ring-httpcore-adapter/project.clj
+++ ring-httpcore-adapter/project.clj
@@ -4,5 +4,4 @@
   :dependencies [[ring/ring-core "0.2.0-SNAPSHOT"]
                  [org.apache.httpcomponents/httpcore "4.0.1"]
                  [org.apache.httpcomponents/httpcore-nio "4.0.1"]]
-  :namespaces :all
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
