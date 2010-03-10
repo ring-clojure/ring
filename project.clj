b6b7ce662c5fc9b83c225bf77799b45a5659f308
Mark McGranaghan <mmcgrana@gmail.com>
2010-03-10 15:08:29 -0500

    Experimental autodoc support.

--- project.clj
+++ project.clj
@@ -5,4 +5,17 @@
                  [ring/ring-devel "0.2.0-SNAPSHOT"]
                  [ring/ring-httpcore-adapter "0.2.0-SNAPSHOT"]
                  [ring/ring-jetty-adapter "0.2.0-SNAPSHOT"]
-                 [ring/ring-servlet "0.2.0-SNAPSHOT"]])
+                 [ring/ring-servlet "0.2.0-SNAPSHOT"]]
+  :dev-dependencies [[autodoc "0.7.0"]]
+  :autodoc
+    {:name "Ring"
+     :description "A Clojure web applications library"
+     :copyright "Copyright 2009-2010 Mark McGranaghan"
+     :root "."
+     :source-path ""
+     :web-src-dir "http://github.com/mmcgrana/ring/blob/"
+     :web-home "http://mmcgrana.github.com/ring/"
+     :output-path "/Users/mmcgrana/Desktop/ring-autodoc"
+     :namespaces-to-document ["ring"]
+     :trim-prefix "ring."
+     :load-except-list [#"/example/" #"/test/" #"project\.clj"]})
