c6f6476bc962cc818c884458ede88b0d09b9aa11
Roman Scherer <roman@soundcloud.com>
2010-06-03 03:19:34 +0800

    Using only copy and throwf from dependent namespaces, which fixes the spit nameclash when using clojure 1.2.

--- ring-servlet/src/ring/util/servlet.clj
+++ ring-servlet/src/ring/util/servlet.clj
@@ -1,6 +1,7 @@
 (ns ring.util.servlet
   "Compatibility functions for turning a ring handler into a Java servlet."
-  (:use (clojure.contrib duck-streams except))
+  (:use [clojure.contrib.except :only (throwf)]
+        [clojure.contrib.duck-streams :only (copy)])
   (:import (java.io File InputStream FileInputStream)
            (javax.servlet.http HttpServlet
                                HttpServletRequest
