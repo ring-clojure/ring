d761fe55586a53f4bccb52332f66165db6b4dd7f
weavejester <jreeves@weavejester.com>
2010-07-16 22:31:55 +0100

    Update ring-core dependency to Clojure 1.2.0-beta1, updated version to 0.3.0-beta1

--- ring-core/project.clj
+++ ring-core/project.clj
@@ -1,8 +1,8 @@
-(defproject ring/ring-core "0.2.5"
+(defproject ring/ring-core "0.3.0-beta1"
   :description "Ring core libraries."
   :url "http://github.com/mmcgrana/ring"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]
+  :dependencies [[org.clojure/clojure "1.2.0-beta1"]
+                 [org.clojure/clojure-contrib "1.2.0-beta1"]
                  [commons-codec "1.4"]
                  [commons-io "1.4"]
                  [commons-fileupload "1.2.1"]
