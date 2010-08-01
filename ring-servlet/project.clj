0961cbd5331b6cddfc26f98c21e0f83a25aaba49
weavejester <jreeves@weavejester.com>
2010-08-01 14:19:19 +0100

    Added swank-clojure to dev dependencies of ring-servlet

--- ring-servlet/project.clj
+++ ring-servlet/project.clj
@@ -3,4 +3,5 @@
   :url "http://github.com/mmcgrana/ring"
   :dependencies [[ring/ring-core "0.3.0-beta1"]
                  [javax.servlet/servlet-api "2.5"]]
-  :dev-dependencies [[lein-clojars "0.5.0"]])
+  :dev-dependencies [[lein-clojars "0.5.0"]
+                     [swank-clojure "1.2.1"]])
