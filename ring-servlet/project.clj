da8efbdd80e05a0b81cec78e2b9a2273a9f162b5
weavejester <jreeves@weavejester.com>
2010-08-01 14:18:48 +0100

    Updated ring-servlet dependencies

--- ring-servlet/project.clj
+++ ring-servlet/project.clj
@@ -1,6 +1,6 @@
-(defproject ring/ring-servlet "0.2.5"
+(defproject ring/ring-servlet "0.3.0-beta1"
   :description "Ring servlet utilities."
   :url "http://github.com/mmcgrana/ring"
-  :dependencies [[ring/ring-core "0.2.5"]
+  :dependencies [[ring/ring-core "0.3.0-beta1"]
                  [javax.servlet/servlet-api "2.5"]]
   :dev-dependencies [[lein-clojars "0.5.0"]])
