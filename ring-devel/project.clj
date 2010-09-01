b6462c6b3487ca0efbc7efb27bd09278cebaa75e
weavejester <jreeves@weavejester.com>
2010-09-01 11:23:22 +0100

    Updated project.clj for ring-devel

--- ring-devel/project.clj
+++ ring-devel/project.clj
@@ -1,7 +1,7 @@
-(defproject ring/ring-devel "0.2.5"
+(defproject ring/ring-devel "0.3.0-beta1"
   :description "Ring development and debugging libraries."
   :url "http://github.com/mmcgrana/ring"
-  :dependencies [[ring/ring-core "0.2.5"]
-                 [hiccup "0.2.6"]
-                 [clj-stacktrace "0.1.0"]]
-  :dev-dependencies [[lein-clojars "0.5.0"]])
+  :dependencies [[ring/ring-core "0.3.0-beta1"]
+                 [hiccup "0.3.0"]
+                 [clj-stacktrace "0.2.0"]]
+  :dev-dependencies [[lein-clojars "0.6.0"]])
