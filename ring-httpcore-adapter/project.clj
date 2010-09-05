e56dd25ca13f5f3068a4a2b97b778a808d0b26cc
weavejester <jreeves@weavejester.com>
2010-09-05 21:58:39 +0100

    Updated project.clj for ring-httpcore-adapter to 0.3.0-beta1

--- ring-httpcore-adapter/project.clj
+++ ring-httpcore-adapter/project.clj
@@ -1,7 +1,7 @@
-(defproject ring/ring-httpcore-adapter "0.2.5"
+(defproject ring/ring-httpcore-adapter "0.3.0-beta1"
   :description "Ring HttpCore adapter."
   :url "http://github.com/mmcgrana/ring"
-  :dependencies [[ring/ring-core "0.2.5"]
+  :dependencies [[ring/ring-core "0.3.0-beta1"]
                  [org.apache.httpcomponents/httpcore "4.0.1"]
                  [org.apache.httpcomponents/httpcore-nio "4.0.1"]]
   :dev-dependencies [[lein-clojars "0.6.0"]])
