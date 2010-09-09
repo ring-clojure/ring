7b8f6bce0b73e27a18f56970f3914481ecbdc151
weavejester <jreeves@weavejester.com>
2010-09-10 00:44:45 +0100

    Release 0.3.0-RC1.

--- README.md
+++ README.md
@@ -91,11 +91,11 @@ To see a more sophisticated Ring app, run:
 
 To include one of the above libraries in your Leiningen project, for example `ring-core`, add the following to your `:dependences`:
 
-    [ring/ring-core "0.2.6"]
+    [ring/ring-core "0.3.0-RC1"]
 
 To include all of them, add:
 
-    [ring "0.2.6"]
+    [ring "0.3.0-RC1"]
 
 ## Development
 
--- ring-jetty-adapter/project.clj
+++ ring-jetty-adapter/project.clj
@@ -1,8 +1,8 @@
-(defproject ring/ring-jetty-adapter "0.3.0-beta1"
+(defproject ring/ring-jetty-adapter "0.3.0-RC1"
   :description "Ring Jetty adapter."
   :url "http://github.com/mmcgrana/ring"
-  :dependencies [[ring/ring-core "0.3.0-beta1"]
-                 [ring/ring-servlet "0.3.0-beta1"]
+  :dependencies [[ring/ring-core "0.3.0-RC1"]
+                 [ring/ring-servlet "0.3.0-RC1"]
                  [org.mortbay.jetty/jetty "6.1.14"]
                  [org.mortbay.jetty/jetty-util "6.1.14"]]
   :dev-dependencies [[lein-clojars "0.6.0"]])
