c0df73857ea84adc9044ec725817512c77009196
weavejester <jreeves@weavejester.com>
2010-11-27 14:11:45 +0000

    Updated Jetty dependencies to 6.1.26

--- ring-jetty-adapter/project.clj
+++ ring-jetty-adapter/project.clj
@@ -3,6 +3,6 @@
   :url "http://github.com/mmcgrana/ring"
   :dependencies [[ring/ring-core "0.3.4"]
                  [ring/ring-servlet "0.3.4"]
-                 [org.mortbay.jetty/jetty "6.1.14"]
-                 [org.mortbay.jetty/jetty-util "6.1.14"]]
+                 [org.mortbay.jetty/jetty "6.1.26"]
+                 [org.mortbay.jetty/jetty-util "6.1.26"]]
   :dev-dependencies [[clj-http "0.1.1"]])
