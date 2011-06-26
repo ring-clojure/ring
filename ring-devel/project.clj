01f19823e8a785c1a32db1fd4becdcaa12e764a1
James Reeves <jreeves@weavejester.com>
2011-06-26 21:00:15 +0100

    Updated Hiccup and clj-stacktrace dependencies

--- ring-devel/project.clj
+++ ring-devel/project.clj
@@ -2,5 +2,5 @@
   :description "Ring development and debugging libraries."
   :url "http://github.com/mmcgrana/ring"
   :dependencies [[ring/ring-core "0.3.8"]
-                 [hiccup "0.3.4"]
-                 [clj-stacktrace "0.2.0"]])
+                 [hiccup "0.3.5"]
+                 [clj-stacktrace "0.2.2"]])
