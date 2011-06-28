685d297929c96f7ac6eccef3843351cdd6047707
James Reeves <jreeves@weavejester.com>
2011-06-28 23:03:03 +0100

    Updated Hiccup to 1.3.0-friendly version 0.3.6

--- ring-devel/project.clj
+++ ring-devel/project.clj
@@ -2,5 +2,5 @@
   :description "Ring development and debugging libraries."
   :url "http://github.com/mmcgrana/ring"
   :dependencies [[ring/ring-core "0.3.9"]
-                 [hiccup "0.3.5"]
+                 [hiccup "0.3.6"]
                  [clj-stacktrace "0.2.2"]])
