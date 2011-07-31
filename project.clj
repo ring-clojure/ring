9feaf239ba88cbf2547553bc1289d13c553659dd
James Reeves <jreeves@weavejester.com>
2011-07-31 14:09:37 +0100

    Updated autodoc to custom working version

--- project.clj
+++ project.clj
@@ -7,17 +7,15 @@
      [ring/ring-jetty-adapter "0.3.11"]
      [ring/ring-servlet "0.3.11"]]
   :dev-dependencies
-    [[autodoc "0.7.1" :exclusions [org.clojure/clojure
-                                   org.clojure/clojure-contrib]]]
+    [[org.clojars.weavejester/autodoc "0.9.0"]]
   :autodoc
     {:name "Ring"
      :description "A Clojure web applications library"
-     :copyright "Copyright 2009-2010 Mark McGranaghan"
+     :copyright "Copyright 2009-2011 Mark McGranaghan"
      :root "."
      :source-path ""
      :web-src-dir "http://github.com/mmcgrana/ring/blob/"
      :web-home "http://mmcgrana.github.com/ring/"
      :output-path "autodoc"
      :namespaces-to-document ["ring"]
-     :trim-prefix "ring."
      :load-except-list [#"/example/" #"/test/" #"project\.clj"]})
