c809b8f3329208f0624f5753bcfe6f0e6f463a17
James Reeves <jreeves@weavejester.com>
2011-10-03 20:58:57 +0100

    Downgraded Jetty from 6.1.26 to 6.1.25 in reponse to socket issue.
    More information here:
    https://groups.google.com/forum/#!topic/ring-clojure/z3n6jqO1fdo

--- ring-jetty-adapter/project.clj
+++ ring-jetty-adapter/project.clj
@@ -3,6 +3,6 @@
   :url "http://github.com/mmcgrana/ring"
   :dependencies [[ring/ring-core "0.3.11"]
                  [ring/ring-servlet "0.3.11"]
-                 [org.mortbay.jetty/jetty "6.1.26"]
-                 [org.mortbay.jetty/jetty-util "6.1.26"]]
+                 [org.mortbay.jetty/jetty "6.1.25"]
+                 [org.mortbay.jetty/jetty-util "6.1.25"]]
   :dev-dependencies [[clj-http "0.1.3"]])
