d2b9783029cc23d1bd5036018334d9545b1d1577
James Reeves <jreeves@weavejester.com>
2011-12-03 14:04:34 +0000

    Release 1.0.0-RC5

--- ring-jetty-adapter/project.clj
+++ ring-jetty-adapter/project.clj
@@ -1,8 +1,8 @@
-(defproject ring/ring-jetty-adapter "1.0.0-RC4"
+(defproject ring/ring-jetty-adapter "1.0.0-RC5"
   :description "Ring Jetty adapter."
   :url "http://github.com/mmcgrana/ring"
-  :dependencies [[ring/ring-core "1.0.0-RC4"]
-                 [ring/ring-servlet "1.0.0-RC4"]
+  :dependencies [[ring/ring-core "1.0.0-RC5"]
+                 [ring/ring-servlet "1.0.0-RC5"]
                  [org.mortbay.jetty/jetty "6.1.25"]
                  [org.mortbay.jetty/jetty-util "6.1.25"]]
   :dev-dependencies [[clj-http "0.1.3"]])
--- ring-servlet/project.clj
+++ ring-servlet/project.clj
@@ -1,5 +1,5 @@
-(defproject ring/ring-servlet "1.0.0-RC4"
+(defproject ring/ring-servlet "1.0.0-RC5"
   :description "Ring servlet utilities."
   :url "http://github.com/mmcgrana/ring"
-  :dependencies [[ring/ring-core "1.0.0-RC4"]
+  :dependencies [[ring/ring-core "1.0.0-RC5"]
                  [javax.servlet/servlet-api "2.5"]])
