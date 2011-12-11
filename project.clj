6ade5c2ff224bb36f118a3cce5493d35b69d0e39
James Reeves <jreeves@weavejester.com>
2011-12-11 17:44:32 +0000

    Replaced autodoc with codox for API doc generation

--- project.clj
+++ project.clj
@@ -7,15 +7,9 @@
      [ring/ring-jetty-adapter "1.0.0-RC5"]
      [ring/ring-servlet "1.0.0-RC5"]]
   :dev-dependencies
-    [[org.clojars.weavejester/autodoc "0.9.0"]]
-  :autodoc
-    {:name "Ring"
-     :description "A Clojure web applications library"
-     :copyright "Copyright 2009-2011 Mark McGranaghan"
-     :root "."
-     :source-path ""
-     :web-src-dir "http://github.com/mmcgrana/ring/blob/"
-     :web-home "http://mmcgrana.github.com/ring/"
-     :output-path "autodoc"
-     :namespaces-to-document ["ring"]
-     :load-except-list [#"/example/" #"/test/" #"project\.clj"]})
+    [[codox "0.3.0"]]
+  :codox
+    {:sources ["ring-core/src"
+               "ring-devel/src"
+               "ring-jetty-adapter/src"
+               "ring-servlet/src"]})
