04ec6e57c769f49fac6191aec3e1a2f69f882938
James Reeves <jreeves@weavejester.com>
2012-01-24 21:00:47 +0000

    Updated clj-stacktrace to 0.2.4 (fixes #50)

--- ring-devel/project.clj
+++ ring-devel/project.clj
@@ -3,5 +3,5 @@
   :url "http://github.com/mmcgrana/ring"
   :dependencies [[ring/ring-core "1.0.1"]
                  [hiccup "0.3.7"]
-                 [clj-stacktrace "0.2.2"]
+                 [clj-stacktrace "0.2.4"]
                  [ns-tracker "0.1.1"]])
