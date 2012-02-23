0c519be7a16b40dacddc3180463ebd69a12679d1
James Reeves <jreeves@weavejester.com>
2012-02-23 23:48:03 +0000

    Updated clj-http dev dependency

--- ring-jetty-adapter/project.clj
+++ ring-jetty-adapter/project.clj
@@ -5,4 +5,4 @@
                  [ring/ring-servlet "1.1.0-SNAPSHOT"]
                  [org.mortbay.jetty/jetty "6.1.25"]
                  [org.mortbay.jetty/jetty-util "6.1.25"]]
-  :dev-dependencies [[clj-http "0.1.3"]])
+  :dev-dependencies [[clj-http "0.3.2"]])
