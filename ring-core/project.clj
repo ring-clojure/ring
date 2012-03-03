cfe0af32702de224cbeb08a1fe5c7cb200348e52
James Reeves <jreeves@weavejester.com>
2012-03-03 18:21:37 +0000

    Updated Apache Commons dependencies

--- ring-core/project.clj
+++ ring-core/project.clj
@@ -2,8 +2,8 @@
   :description "Ring core libraries."
   :url "http://github.com/mmcgrana/ring"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [commons-codec "1.4"]
-                 [commons-io "1.4"]
+                 [commons-codec "1.6"]
+                 [commons-io "2.1"]
                  [commons-fileupload "1.2.1"]
                  [javax.servlet/servlet-api "2.5"]]
   :dev-dependencies [[org.clojure/clojure-contrib "1.2.0"]])
