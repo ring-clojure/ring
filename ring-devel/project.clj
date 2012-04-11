c1cf66d59803a2b6443d2166dc5810e98d20704f
James Reeves <jreeves@weavejester.com>
2012-04-11 18:41:33 +0100

    Updated Hiccup to 1.0.0-RC2

--- ring-devel/project.clj
+++ ring-devel/project.clj
@@ -2,6 +2,6 @@
   :description "Ring development and debugging libraries."
   :url "http://github.com/mmcgrana/ring"
   :dependencies [[ring/ring-core "1.1.0-beta3"]
-                 [hiccup "1.0.0-RC1"]
+                 [hiccup "1.0.0-RC2"]
                  [clj-stacktrace "0.2.4"]
                  [ns-tracker "0.1.1"]])
