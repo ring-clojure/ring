514b49f48bfa8e921b3788ad8299359ed83fdd00
Joe Snikeris <joe@snikeris.com>
2012-08-23 00:34:40 -0400

    add request-url fn to new ring.util.request ns
    add tests for request-url

--- ring-core/src/ring/util/request.clj
+++ ring-core/src/ring/util/request.clj
@@ -1,7 +1,7 @@
-(ns ring.middleware.absolute-uri
-  "Middleware for adding absolute URI of request to request map.")
+(ns ring.util.request
+  "Functions that operate on Ring requests")
 
-(defn- absolute-uri [req]
+(defn request-url [req]
   (let [scheme (-> req :scheme name)
         host (:server-name req)
         port (:server-port req)
@@ -9,8 +9,3 @@
         path (:uri req)
         query (when-let [q (:query-string req)] (str "?" q))]
     (str scheme "://" host port-part path query)))
-
-(defn wrap-absolute-uri [handler]
-  (fn [request]
-    (handler (assoc request :absolute-uri (absolute-uri request)))))
-
---
+++ ring-core/test/ring/util/test/request.clj
@@ -0,0 +1,18 @@
+(ns ring.util.test.request
+  (:use clojure.test
+        ring.util.request))
+
+(def base-req
+  {:server-port 80
+   :server-name "example.com"
+   :uri "/"
+   :query-string nil
+   :scheme :http})
+
+(deftest request-url-test
+  (are [req url] (= (request-url req) url)
+       base-req "http://example.com/"
+       (assoc base-req :server-port 8080) "http://example.com:8080/"
+       (assoc base-req :uri "/abc/def") "http://example.com/abc/def"
+       (assoc base-req :query-string "a=b&c=d") "http://example.com/?a=b&c=d"
+       (assoc base-req :scheme :https) "https://example.com/"))
