4c0d8f4ce8b0e573d8459f9bf9a748828e1fdf56
Joe Snikeris <joe@snikeris.com>
2012-08-27 16:58:56 -0400

    add doc-strings to fns

--- ring-core/src/ring/util/request.clj
+++ ring-core/src/ring/util/request.clj
@@ -3,6 +3,7 @@
   (:require [clojure.string :as str]))
 
 (defn base-url [req]
+  "Returns a request's base URL, e.g. http://example.com/"
   (let [port (:server-port req)]
     (str (-> req :scheme name)
          "://"
@@ -11,6 +12,7 @@
          "/")))
 
 (defn request-url [req]
+  "Returns a request's URL, e.g. http://example.com/a/b/c?d=f"
   (str (-> req base-url (str/replace-first #"/$" ""))
        (:uri req)
        (when-let [q (:query-string req)] (str "?" q))))
