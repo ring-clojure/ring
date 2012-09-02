0ec36561858980996d69aec30cc5f7d23d1af1b7
James Reeves <jreeves@weavejester.com>
2012-09-02 15:07:29 +0100

    Updated ns-tracker to fix wrap-reload bug

--- ring-devel/project.clj
+++ ring-devel/project.clj
@@ -4,4 +4,4 @@
   :dependencies [[ring/ring-core "1.1.3"]
                  [hiccup "1.0.0"]
                  [clj-stacktrace "0.2.4"]
-                 [ns-tracker "0.1.1"]])
+                 [ns-tracker "0.1.2"]])
