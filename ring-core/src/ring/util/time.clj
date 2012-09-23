3b802aa9f885cdce383766131b3f531221670cf1
James Reeves <jreeves@weavejester.com>
2012-09-23 18:12:39 +0100

    Fixed parse-date bug with Clojure 1.3

--- ring-core/src/ring/util/time.clj
+++ ring-core/src/ring/util/time.clj
@@ -18,7 +18,10 @@
 (defn- attempt-parse [date format]
   (try
     (.parse (formatter format) date)
-    (catch ParseException _ nil)))
+    (catch ParseException _ nil)
+    ;; Also catch RuntimeExceptions, needed for Clojure 1.3.0
+    ;; See: https://groups.google.com/forum/#!topic/clojure/I5l1YHVMgkI
+    (catch RuntimeException _ nil)))
 
 (defn- trim-quotes [s]
   (str/replace s #"^'|'$" ""))
