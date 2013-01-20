a810e4fa65f10daeb31dd6a8e08182d0031ecc9e
James Reeves <jreeves@weavejester.com>
2013-01-20 16:30:20 +0000

    Updated clj-stacktrace dependency

--- ring-devel/project.clj
+++ ring-devel/project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/ring-clojure/ring"
   :dependencies [[ring/ring-core "1.1.7"]
                  [hiccup "1.0.0"]
-                 [clj-stacktrace "0.2.4"]
+                 [clj-stacktrace "0.2.5"]
                  [ns-tracker "0.2.1"]])
