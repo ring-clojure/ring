f634d74f5776fc99d094a5bb24facfe3f60c8523
Gabriel Horner <gabriel.horner@gmail.com>
2013-01-22 14:54:07 -0500

    separate out response for content-type middleware
    also add test for :mime-options

--- ring-core/test/ring/middleware/test/content_type.clj
+++ ring-core/test/ring/middleware/test/content_type.clj
@@ -25,6 +25,12 @@
       (is (= (handler {:uri "/foo/bar"})
              {:headers {"Content-Type" "application/octet-stream"}}))))
 
+  (testing "response with mime-types option"
+    (let [response {:headers {}}
+          handler  (wrap-content-type (constantly response) {:mime-types {"edn" "application/edn"}})]
+      (is (= (handler {:uri "/all.edn"})
+             {:headers {"Content-Type" "application/edn"}}))))
+
   (testing "nil response"
     (let [handler (wrap-content-type (constantly nil))]
       (is (nil? (handler {:uri "/foo/bar.txt"}))))))
