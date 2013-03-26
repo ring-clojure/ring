4ae28733daaa9a8a2cfb9e2bc54e3d453cb74f32
Herwig Hochleitner <hhochleitner@gmail.com>
2013-03-26 01:29:21 +0100

    Bump to clojure version 1.5.1 in 1.5 profile

--- ring-core/project.clj
+++ ring-core/project.clj
@@ -11,4 +11,4 @@
                  [clj-time "0.4.4"]]
   :profiles
   {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-   :1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]}})
+   :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}})
