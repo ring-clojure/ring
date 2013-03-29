f6e809cb32452fb018de0dab5f76198bb1cdaf71
James Reeves <jreeves@weavejester.com>
2013-03-29 16:14:20 +0000

    Fixed indentation in ring.middleware.test.not-modified

--- ring-core/test/ring/middleware/test/not_modified.clj
+++ ring-core/test/ring/middleware/test/not_modified.clj
@@ -29,21 +29,21 @@
 
 (deftest test-not-modified-response  
   (testing "etag match"
-           (let [known-etag "known-etag"
-                 request {:headers {"if-none-match" known-etag}}
-                 handler-resp #(hash-map :status 200 :headers {"etag" %} :body "")]
-             (is (= 304 (:status (not-modified-response (handler-resp known-etag) request))))
-             (is (= 200 (:status (not-modified-response (handler-resp "unknown-etag") request))))))
+    (let [known-etag "known-etag"
+          request {:headers {"if-none-match" known-etag}}
+          handler-resp #(hash-map :status 200 :headers {"etag" %} :body "")]
+      (is (= 304 (:status (not-modified-response (handler-resp known-etag) request))))
+      (is (= 200 (:status (not-modified-response (handler-resp "unknown-etag") request))))))
   
   (testing "not modified"
-           (let [req #(hash-map :headers {"if-modified-since" %})
-                 last-modified "Sun, 23 Sep 2012 11:00:00 GMT"
-                 h-resp {:status 200 :headers {"Last-Modified" last-modified} :body ""}]
-             (is (= 304 (:status (not-modified-response h-resp (req last-modified)))))
-             (is (= 304 (:status (not-modified-response h-resp (req "Sun, 23 Sep 2012 11:52:50 GMT")))))
-             (is (= 200 (:status (not-modified-response h-resp (req "Sun, 23 Sep 2012 10:00:50 GMT")))))))
+    (let [req #(hash-map :headers {"if-modified-since" %})
+          last-modified "Sun, 23 Sep 2012 11:00:00 GMT"
+          h-resp {:status 200 :headers {"Last-Modified" last-modified} :body ""}]
+      (is (= 304 (:status (not-modified-response h-resp (req last-modified)))))
+      (is (= 304 (:status (not-modified-response h-resp (req "Sun, 23 Sep 2012 11:52:50 GMT")))))
+      (is (= 200 (:status (not-modified-response h-resp (req "Sun, 23 Sep 2012 10:00:50 GMT")))))))
   
   (testing "no modification info"
-           (let [response {:status 200 :headers {} :body ""}]
-             (is (= 200 (:status (not-modified-response response (etag-request "\"12345\"")))))
-             (is (= 200 (:status (not-modified-response response (modified-request "Sun, 23 Sep 2012 10:00:00 GMT"))))))))
+    (let [response {:status 200 :headers {} :body ""}]
+      (is (= 200 (:status (not-modified-response response (etag-request "\"12345\"")))))
+      (is (= 200 (:status (not-modified-response response (modified-request "Sun, 23 Sep 2012 10:00:00 GMT"))))))))
