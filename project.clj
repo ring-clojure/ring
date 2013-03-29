831a3f64b9bd09b29fc94715503685aede1bab82
James Reeves <jreeves@weavejester.com>
2013-03-29 20:00:25 +0000

    Released 1.2.0-beta2

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject ring "1.2.0-beta1"
+(defproject ring "1.2.0-beta2"
   :description "A Clojure web applications library."
   :url "https://github.com/ring-clojure/ring"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies
-    [[ring/ring-core "1.2.0-beta1"]
-     [ring/ring-devel "1.2.0-beta1"]
-     [ring/ring-jetty-adapter "1.2.0-beta1"]
-     [ring/ring-servlet "1.2.0-beta1"]]
+    [[ring/ring-core "1.2.0-beta2"]
+     [ring/ring-devel "1.2.0-beta2"]
+     [ring/ring-jetty-adapter "1.2.0-beta2"]
+     [ring/ring-servlet "1.2.0-beta2"]]
   :plugins
     [[lein-sub "0.2.1"]
      [codox "0.6.4"]]
--- ring-devel/project.clj
+++ ring-devel/project.clj
@@ -1,9 +1,9 @@
-(defproject ring/ring-devel "1.2.0-beta1"
+(defproject ring/ring-devel "1.2.0-beta2"
   :description "Ring development and debugging libraries."
   :url "https://github.com/ring-clojure/ring"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[ring/ring-core "1.2.0-beta1"]
+  :dependencies [[ring/ring-core "1.2.0-beta2"]
                  [hiccup "1.0.3"]
                  [clj-stacktrace "0.2.5"]
                  [ns-tracker "0.2.1"]]
