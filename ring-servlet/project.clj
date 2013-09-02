7b5fbf2e362140ce29e21483a14b085f2f302a7e
Jesper André Lyngesen Pedersen <jalpedersen@gmail.com>
2013-09-02 21:44:49 +0200

    Make javax.servlet a provided dependency This is so we can run in a servlet 3.0 environment as well (Jetty 8+)

--- ring-servlet/project.clj
+++ ring-servlet/project.clj
@@ -3,8 +3,9 @@
   :url "https://github.com/ring-clojure/ring"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[ring/ring-core "1.2.0"]
-                 [javax.servlet/servlet-api "2.5"]]
+  :dependencies [[ring/ring-core "1.2.0"]]
   :profiles
-  {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+  {:provided {:dependencies [[javax.servlet/servlet-api "2.5"]]}
+   :dev {:dependencies [[javax.servlet/servlet-api "2.5"]]}
+   :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
    :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}})
