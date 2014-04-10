48f12f798133df0c116177776e03882529cd059f
James Reeves <jreeves@weavejester.com>
2014-04-10 16:14:51 +0100

    Updated clj-stacktrace to 0.2.7

--- ring-devel/project.clj
+++ ring-devel/project.clj
@@ -5,7 +5,7 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[ring/ring-core "1.2.2"]
                  [hiccup "1.0.3"]
-                 [clj-stacktrace "0.2.5"]
+                 [clj-stacktrace "0.2.7"]
                  [ns-tracker "0.2.2"]]
   :profiles
   {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
