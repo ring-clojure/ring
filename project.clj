af0379bd6f2239867e3c5ec5d325224b520aadf9
James Reeves <jreeves@weavejester.com>
2014-05-19 15:22:04 +0100

    Released 1.3.0-RC1

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject ring "1.3.0-beta2"
+(defproject ring "1.3.0-RC1"
   :description "A Clojure web applications library."
   :url "https://github.com/ring-clojure/ring"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies
     [[org.clojure/clojure "1.3.0"]
-     [ring/ring-core "1.3.0-beta2"]
-     [ring/ring-devel "1.3.0-beta2"]
-     [ring/ring-jetty-adapter "1.3.0-beta2"]
-     [ring/ring-servlet "1.3.0-beta2"]]
+     [ring/ring-core "1.3.0-RC1"]
+     [ring/ring-devel "1.3.0-RC1"]
+     [ring/ring-jetty-adapter "1.3.0-RC1"]
+     [ring/ring-servlet "1.3.0-RC1"]]
   :plugins
     [[lein-sub "0.2.4"]
      [codox "0.8.5"]]
@@ -18,7 +18,7 @@
      "ring-jetty-adapter"
      "ring-servlet"]
   :codox
-    {:src-dir-uri "http://github.com/ring-clojure/ring/blob/1.2.2/"
+    {:src-dir-uri "http://github.com/ring-clojure/ring/blob/1.3.0-RC1/"
      :src-linenum-anchor-prefix "L"
      :sources ["ring-core/src"
                "ring-devel/src"
--- ring-core/project.clj
+++ ring-core/project.clj
@@ -1,4 +1,4 @@
-(defproject ring/ring-core "1.3.0-beta2"
+(defproject ring/ring-core "1.3.0-RC1"
   :description "Ring core libraries."
   :url "https://github.com/ring-clojure/ring"
   :scm {:dir ".."}
