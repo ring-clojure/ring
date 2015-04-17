29c741e595ddff5729d4b91fb1a5af3c0ea7caeb
James Reeves <jreeves@weavejester.com>
2015-04-17 17:33:22 +0100

    Update Apache Commons FileUpload to 1.3.1

--- ring-core/project.clj
+++ ring-core/project.clj
@@ -8,7 +8,7 @@
                  [org.clojure/tools.reader "0.8.1"]
                  [ring/ring-codec "1.0.0"]
                  [commons-io "2.4"]
-                 [commons-fileupload "1.3"]
+                 [commons-fileupload "1.3.1"]
                  [clj-time "0.9.0"]
                  [crypto-random "1.2.0"]
                  [crypto-equality "1.0.0"]]
