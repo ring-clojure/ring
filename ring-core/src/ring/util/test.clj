ab7b7aab334b17d79dbd605aab1b69669695e25c
James Reeves <jreeves@weavejester.com>
2015-07-02 12:04:53 +0100

    Add deprecated metadata to ring.util.test

--- ring-core/src/ring/util/test.clj
+++ ring-core/src/ring/util/test.clj
@@ -2,6 +2,7 @@
   "Utilities for testing Ring components.
 
   All functions in this namespace are currently deprecated."
+  {:deprecated "1.1"}
   (:require [ring.util.io :as io]))
 
 (def ^{:doc "Returns a ByteArrayInputStream for the given String.
