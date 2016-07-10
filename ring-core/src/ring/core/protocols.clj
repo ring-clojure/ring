66c20ea7df28bc1217df86c8a5484588e6d3f7b9
James Reeves <jreeves@weavejester.com>
2016-07-10 02:20:38 +0100

    Improve docstrings and metadata for ring.core.protocols

--- ring-core/src/ring/core/protocols.clj
+++ ring-core/src/ring/core/protocols.clj
@@ -1,10 +1,13 @@
 (ns ring.core.protocols
+  "Protocols necessary for Ring."
+  {:added "1.6"}
   (:require [clojure.java.io :as io]))
 
-(defprotocol StreamingResponseBody
+(defprotocol ^{:added "1.6"} StreamingResponseBody
   "A protocol for writing data to the response body via an output stream."
   (write-body-to-stream [body output-stream]
-    "Write a value representing a response body to an output stream."))
+    "Write a value representing a response body to an output stream. The stream
+    will be closed after the value had been written."))
 
 (extend-protocol StreamingResponseBody
   String
