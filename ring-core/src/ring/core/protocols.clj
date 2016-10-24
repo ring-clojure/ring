975b93928b6de5969293af2a78f2efb0a87932b7
Tommi Reiman <tommi@iki.fi>
2016-10-24 16:20:35 +0200

    Fix reflection warnings

--- ring-core/src/ring/core/protocols.clj
+++ ring-core/src/ring/core/protocols.clj
@@ -1,6 +1,7 @@
 (ns ring.core.protocols
   "Protocols necessary for Ring."
   {:added "1.6"}
+  (:import [java.io Writer OutputStream])
   (:require [clojure.java.io :as io]
             [ring.util.response :as response]))
 
@@ -10,7 +11,7 @@
     "Write a value representing a response body to an output stream. The stream
     will be closed after the value had been written."))
 
-(defn- response-writer [response output-stream]
+(defn- ^Writer response-writer [response output-stream]
   (if-let [charset (response/get-charset response)]
     (io/writer output-stream :encoding charset)
     (io/writer output-stream)))
@@ -26,11 +27,11 @@
       (doseq [chunk body]
         (.write writer (str chunk)))))
   java.io.InputStream
-  (write-body-to-stream [body _ output-stream]
+  (write-body-to-stream [body _ ^OutputStream output-stream]
     (with-open [out output-stream, body body]
       (io/copy body out)))
   java.io.File
-  (write-body-to-stream [body _ output-stream]
+  (write-body-to-stream [body _ ^OutputStream output-stream]
     (with-open [out output-stream]
       (io/copy body out)))
   nil
