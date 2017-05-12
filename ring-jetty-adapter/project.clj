0539e0ca241017e20084c30b6f36415064091ccd
James Reeves <jreeves@weavejester.com>
2017-05-12 15:59:46 +0100

    Update Jetty to 9.2.21.v201701209

--- ring-jetty-adapter/project.clj
+++ ring-jetty-adapter/project.clj
@@ -7,7 +7,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring/ring-core "1.6.0"]
                  [ring/ring-servlet "1.6.0"]
-                 [org.eclipse.jetty/jetty-server "9.2.17.v20160517"]]
+                 [org.eclipse.jetty/jetty-server "9.2.21.v20170120"]]
   :profiles
   {:dev {:dependencies [[clj-http "2.2.0"]]}
    :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
