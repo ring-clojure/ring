d85a6334f49231ba799c88c5f6bd21b72cfb52a4
xandrews <john@neo.com>
2017-12-08 17:46:39 +0000

    Add namespaces to wrap-keyword-params

--- ring-core/src/ring/middleware/keyword_params.clj
+++ ring-core/src/ring/middleware/keyword_params.clj
@@ -1,38 +1,55 @@
 (ns ring.middleware.keyword-params
   "Middleware that converts parameter keys in the request to keywords.")
 
-(defn- keyword-syntax? [s]
-  (re-matches #"[\p{L}*+!_?-][\p{L}0-9*+!_?-]*" s))
+(def ^:private re-plain-keyword
+  #"[\p{L}*+!_?-][\p{L}\d*+!_?-]*")
 
-(defn- keyify-params [target]
+(def ^:private re-namespaced-keyword
+  #"[\p{L}*+!_?-][\p{L}\d*+!_?.-]*/[\p{L}*+!_?-][\p{L}\d*+!_?-]*")
+
+(defn- keyword-syntax?
+  [s parse-namespaces?]
+  (or (re-matches re-plain-keyword s)
+      (if parse-namespaces? (re-matches re-namespaced-keyword s))))
+
+(defn- keyify-params [target parse-namespaces?]
   (cond
     (map? target)
-      (into {}
-        (for [[k v] target]
-          [(if (and (string? k) (keyword-syntax? k))
-             (keyword k)
-             k)
-           (keyify-params v)]))
+    (into {}
+          (for [[k v] target]
+            [(if (and (string? k) (keyword-syntax? k parse-namespaces?))
+               (keyword k)
+               k)
+             (keyify-params v parse-namespaces?)]))
     (vector? target)
-      (vec (map keyify-params target))
+    (vec (map #(keyify-params % parse-namespaces?) target))
     :else
-      target))
+    target))
 
 (defn keyword-params-request
   "Converts string keys in :params map to keywords. See: wrap-keyword-params."
   {:added "1.2"}
-  [request]
-  (update-in request [:params] keyify-params))
+  ([request]
+   (keyword-params-request request {}))
+  ([request options]
+   (update-in request [:params] keyify-params (:parse-namespaces? options false))))
 
 (defn wrap-keyword-params
   "Middleware that converts the any string keys in the :params map to keywords.
   Only keys that can be turned into valid keywords are converted.
 
   This middleware does not alter the maps under :*-params keys. These are left
-  as strings."
-  [handler]
-  (fn
-    ([request]
-     (handler (keyword-params-request request)))
-    ([request respond raise]
-     (handler (keyword-params-request request) respond raise))))
+  as strings.
+
+  Accepts the following options:
+
+  :parse-namespaces? - if true, parse the parameters into namespaced keywords
+                       (defaults to false)"
+  ([handler]
+   (wrap-keyword-params handler {}))
+  ([handler options]
+   (fn
+     ([request]
+      (handler (keyword-params-request request options)))
+     ([request respond raise]
+      (handler (keyword-params-request request options) respond raise)))))
--- ring-core/test/ring/middleware/test/keyword_params.clj
+++ ring-core/test/ring/middleware/test/keyword_params.clj
@@ -2,24 +2,39 @@
   (:require [clojure.test :refer :all]
             [ring.middleware.keyword-params :refer :all]))
 
-(def wrapped-echo (wrap-keyword-params identity))
+(def wrapped-echo         (wrap-keyword-params identity))
+(def wrapped-echo-with-ns (wrap-keyword-params identity {:parse-namespaces? true}))
 
 (deftest test-wrap-keyword-params
-  (are [in out] (= out (:params (wrapped-echo {:params in})))
-    {"foo" "bar" "biz" "bat"}
-    {:foo  "bar" :biz  "bat"}
-    {"foo" "bar" "biz" [{"bat" "one"} {"bat" "two"}]}
-    {:foo  "bar" :biz  [{:bat "one"}  {:bat  "two"}]}
-    {"foo" 1}
-    {:foo  1}
-    {"foo" 1 "1bar" 2 "baz*" 3 "quz-buz" 4 "biz.bang" 5}
-    {:foo 1 "1bar" 2 :baz* 3 :quz-buz 4 "biz.bang" 5}
-    {:foo "bar"}
-    {:foo "bar"}
-    {"foo" {:bar "baz"}}
-    {:foo {:bar "baz"}}
-    {"fòö" "bar" "bíz" "bat"}
-    {:fòö  "bar" :bíz  "bat"}))
+  (testing "plain keywords"
+    (are [in out] (= out (:params (wrapped-echo {:params in})))
+      {"foo" "bar" "biz" "bat"}
+      {:foo "bar" :biz "bat"}
+      {"foo" "bar" "biz" [{"bat" "one"} {"bat" "two"}]}
+      {:foo "bar" :biz [{:bat "one"}  {:bat "two"}]}
+      {"foo" 1}
+      {:foo 1}
+      {"foo" 1 "1bar" 2 "baz*" 3 "quz-buz" 4 "biz.bang" 5}
+      {:foo 1 "1bar" 2 :baz* 3 :quz-buz 4 "biz.bang" 5}
+      {:foo "bar"}
+      {:foo "bar"}
+      {"foo" {:bar "baz"}}
+      {:foo {:bar "baz"}}
+      {"fòö" "bar" "bíz" "bat"}
+      {:fòö  "bar" :bíz  "bat"}
+      {"ns/foo" "bar"}
+      {"ns/foo" "bar"}))
+
+  (testing "namespaced keywords"
+    (are [in out] (= out (:params (wrapped-echo-with-ns {:params in})))
+      {"ns/foo" "bar" "ns/wtf/foo" "baz" "foo/1" "fizz" "foo/bar1" "buzz"}
+      {:ns/foo "bar" "ns/wtf/foo" "baz" "foo/1" "fizz" :foo/bar1 "buzz"}
+      {"dotted.ns/foo" "bar" "dotted.ns/1" "baz" "dotted.ns/bar1" "buzz"}
+      {:dotted.ns/foo "bar" "dotted.ns/1" "baz" :dotted.ns/bar1 "buzz"}
+      {"fò/bö" "bar" "a.b.í/z" "bat"}
+      {:fò/bö  "bar" :a.b.í/z  "bat"}
+      {"foo" "bar"}
+      {:foo "bar"})))
 
 (deftest wrap-keyword-params-cps-test
   (let [handler   (wrap-keyword-params (fn [req respond _] (respond (:params req))))
