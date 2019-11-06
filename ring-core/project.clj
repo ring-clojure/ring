cfdba94fddf734b3fbbb9c454dbbed452686357b
James Reeves <jreeves@weavejester.com>
2019-11-06 20:23:54 +0000

    Update ring-core dependencies

--- ring-core/project.clj
+++ ring-core/project.clj
@@ -5,15 +5,15 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [ring/ring-codec "1.1.1"]
+                 [ring/ring-codec "1.1.2"]
                  [commons-io "2.6"]
-                 [commons-fileupload "1.3.3"]
+                 [commons-fileupload "1.4"]
                  [crypto-random "1.2.0"]
                  [crypto-equality "1.0.0"]]
   :aliases {"test-all" ["with-profile" "default:+1.8:+1.9:+1.10" "test"]}
   :profiles
   {:provided {:dependencies [[javax.servlet/servlet-api "2.5"]]}
-   :dev  {:dependencies [[clj-time "0.14.3"]
+   :dev  {:dependencies [[clj-time "0.15.2"]
                         [javax.servlet/servlet-api "2.5"]]}
    :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
    :1.9  {:dependencies [[org.clojure/clojure "1.9.0"]]}
