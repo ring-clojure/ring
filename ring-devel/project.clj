f99fd212cd52b0841f5e240e56b1a03a5b8db790
James Reeves <jreeves@weavejester.com>
2020-03-08 16:43:02 +0000

    Update minimum Clojure version to 1.8.0

--- ring-devel/project.clj
+++ ring-devel/project.clj
@@ -4,13 +4,12 @@
   :scm {:dir ".."}
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring/ring-core "1.8.0"]
                  [hiccup "1.0.5"]
                  [clj-stacktrace "0.2.8"]
                  [ns-tracker "0.4.0"]]
-  :aliases {"test-all" ["with-profile" "default:+1.8:+1.9:+1.10" "test"]}
+  :aliases {"test-all" ["with-profile" "default:+1.9:+1.10" "test"]}
   :profiles
-  {:1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
-   :1.9  {:dependencies [[org.clojure/clojure "1.9.0"]]}
+  {:1.9  {:dependencies [[org.clojure/clojure "1.9.0"]]}
    :1.10 {:dependencies [[org.clojure/clojure "1.10.1"]]}})
