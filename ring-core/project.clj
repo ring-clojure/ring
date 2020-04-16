276011fa112193073e0e022cab53ad0c38410dc0
James Reeves <jreeves@weavejester.com>
2020-04-16 18:27:19 +0100

    Update all subprojects to Clojure 1.9.0

--- ring-core/project.clj
+++ ring-core/project.clj
@@ -4,16 +4,15 @@
   :scm {:dir ".."}
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [ring/ring-codec "1.1.2"]
                  [commons-io "2.6"]
                  [commons-fileupload "1.4"]
                  [crypto-random "1.2.0"]
                  [crypto-equality "1.0.0"]]
-  :aliases {"test-all" ["with-profile" "default:+1.9:+1.10" "test"]}
+  :aliases {"test-all" ["with-profile" "default:+1.10" "test"]}
   :profiles
   {:provided {:dependencies [[javax.servlet/servlet-api "2.5"]]}
    :dev  {:dependencies [[clj-time "0.15.2"]
                         [javax.servlet/servlet-api "2.5"]]}
-   :1.9  {:dependencies [[org.clojure/clojure "1.9.0"]]}
    :1.10 {:dependencies [[org.clojure/clojure "1.10.1"]]}})
