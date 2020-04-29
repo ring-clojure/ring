e172d4b024d9c54577f197efb263a4e4aeb9efcc
James Reeves <jreeves@weavejester.com>
2020-04-29 02:18:04 +0100

    Add ring-bench subproject
    Contains the beginnings of benchmarking tests for Ring.

---
+++ ring-bench/project.clj
@@ -0,0 +1,12 @@
+(defproject ring/ring-bench "1.8.0"
+  :description "Ring core libraries."
+  :url "https://github.com/ring-clojure/ring"
+  :scm {:dir ".."}
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [jmh-clojure "0.2.1"]
+                 [ring/ring-jetty-adapter "1.8.0"]
+                 [ring/ring-servlet "1.8.0"]]
+  :jvm-opts {}
+  :main ring.bench.servlet)
---
+++ ring-bench/src/ring/bench/servlet.clj
@@ -0,0 +1,47 @@
+(ns ring.bench.servlet
+  (:require [clojure.pprint :as pp]
+            [jmh.core :as jmh]
+            [ring.util.servlet :as servlet])
+  (:import [java.util HashMap ArrayList]))
+
+(defn http-servlet-request []
+  (let [headers (HashMap.
+                 {"Content-Type"    (ArrayList. ["application/json"])
+                  "Host"            (ArrayList. ["localhost"])
+                  "User-Agent"      (ArrayList. ["MockAgent/1.0.0"])
+                  "Accept"          (ArrayList. ["application/json" "*/*"])
+                  "Accept-Encoding" (ArrayList. ["gzip, deflate"])
+                  "Connection"      (ArrayList. ["keep-alive"])
+                  "Content-Length"  (ArrayList. ["14"])})]
+    (reify javax.servlet.http.HttpServletRequest
+      (getServerPort        [_] 8080)
+      (getServerName        [_] "localhost")
+      (getRemoteAddr        [_] "localhost")
+      (getRequestURI        [_] "/example")
+      (getQueryString       [_] "q=test") 
+      (getScheme            [_] "http")
+      (getMethod            [_] "GET")
+      (getProtocol          [_] "HTTP/1.1")
+      (getHeaderNames       [_]   (java.util.Collections/enumeration (.keySet headers)))
+      (getHeaders           [_ k] (java.util.Collections/enumeration (.get headers k)))
+      (getAttribute         [_ _] nil)
+      (getContentType       [_] "application/json")
+      (getContentLength     [_] 14)
+      (getCharacterEncoding [_] nil)
+      (getInputStream       [_] (proxy [javax.servlet.ServletInputStream] [])))))
+
+(def bench-env
+  {:benchmarks
+   [{:name :build-test, :fn `servlet/build-request-map, :args [:state/request]}]
+   :states
+   {:request {:fn `http-servlet-request, :args []}}})
+
+(def bench-opts
+  {:type :quick
+   :profilers ["stack"]})
+
+(defn -main []
+  (println "Benchmarking...")
+  (doseq [result (jmh/run bench-env bench-opts)]
+    (let [[score unit] (:score result)]
+      (println (format "  %s - %.2f %s" (:fn result) score unit)))))
