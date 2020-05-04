31ba4173919a0d85cebe62364d868bef396d3c17
James Reeves <jreeves@weavejester.com>
2020-05-04 05:08:57 +0100

    Add request and response protocols

--- ring-core/src/ring/request.clj
+++ ring-core/src/ring/request.clj
@@ -3,20 +3,52 @@
   {:added "2.0"}
   (:require [ring.util.parsing :as parsing]))
 
+(defprotocol Request
+  "A protocol representing a HTTP request. By default satisfied by maps that
+  use either Ring 1 or Ring 2 keys. May be extended to provide performant
+  access to underlying request objects without the need for a map lookup."
+  (server-port     [req])
+  (server-name     [req])
+  (remote-addr     [req])
+  (ssl-client-cert [req])
+  (method          [req])
+  (scheme          [req])
+  (path            [req])
+  (query           [req])
+  (protocol        [req])
+  (headers         [req])
+  (body            [req])
+  (get-header [req name]))
+
 (defprotocol StreamableRequestBody
   "A protocol for reading the request body as an input stream."
   (-body-stream [body request]))
 
 (defn ^java.io.InputStream body-stream
-  "Given a Ring 2 request map, return an input stream to read the body."
+  "Given a request map, return an input stream to read the body."
   [request]
-  (-body-stream (::body request) request))
+  (-body-stream (body request) request))
 
 (defn charset
   "Given a request map, return the charset of the content-type header."
   [request]
   (when-let [content-type (get-header request "content-type")]
     (second (re-find parsing/re-charset content-type))))
+
+(extend-protocol Request
+  clojure.lang.IPersistentMap
+  (server-port     [req] (::server-port     req (:server-port req)))
+  (server-name     [req] (::server-name     req (:server-name req)))
+  (remote-addr     [req] (::remote-addr     req (:remote-addr req)))
+  (ssl-client-cert [req] (::ssl-client-cert req (:ssl-client-cert req)))
+  (method          [req] (::method          req (:request-method req)))
+  (scheme          [req] (::scheme          req (:scheme req)))
+  (path            [req] (::path            req (:uri req)))
+  (query           [req] (::query           req (:query-string req)))
+  (protocol        [req] (::protocol        req (:protocol req)))
+  (headers         [req] (::headers         req (:headers req)))
+  (body            [req] (::body            req (:body req)))
+  (get-header [req name] (get (headers req) name)))
 
 (extend-protocol StreamableRequestBody
   (Class/forName "[B")
--- ring-core/src/ring/response.clj
+++ ring-core/src/ring/response.clj
@@ -4,6 +4,13 @@
   (:require [clojure.java.io :as io]
             [ring.util.parsing :as parsing]))
 
+(defprotocol Response
+  "A protocol representing a HTTP response. By default satisfied by maps that
+  use either Ring 1 or Ring 2 keys."
+  (status  [resp])
+  (headers [resp])
+  (body    [resp]))
+
 (defprotocol StreamableResponseBody
   "A protocol for writing data to the response body via an output stream."
   (-write-body-to-stream [body response output-stream]))
@@ -12,7 +19,7 @@
   "Write a value representing a response body to an output stream. The stream
   will be automically closed after the function ends."
   [response output-stream]
-  (-write-body-to-stream (::body response) response output-stream))
+  (-write-body-to-stream (body response) response output-stream))
 
 (defn charset
   "Given a response map, return the charset of the content-type header."
@@ -24,6 +31,12 @@
   (if-let [encoding (charset response)]
     (io/writer output-stream :encoding encoding)
     (io/writer output-stream)))
+
+(extend-protocol Response
+  clojure.lang.IPersistentMap
+  (status  [resp] (::status  resp (:status resp)))
+  (headers [resp] (::headers resp (:headers resp)))
+  (body    [resp] (::body    resp (:body resp))))
 
 (extend-protocol StreamableResponseBody
   (Class/forName "[B")
