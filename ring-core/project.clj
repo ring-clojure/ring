46c49d821beab80592a32cf3f44edc3308fc6383
Patrick Tsai <patosai@users.noreply.github.com>
2020-11-22 13:00:52 -0800

    Use Instaparse to parse header

--- ring-core/project.clj
+++ ring-core/project.clj
@@ -5,13 +5,13 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/spec.alpha "0.2.187"]
                  [ring/ring-codec "1.1.2"]
                  [commons-io "2.6"]
                  [commons-fileupload "1.4"]
                  [crypto-random "1.2.0"]
-                 [crypto-equality "1.0.0"]]
-  :aliases {"test-all" ["with-profile" "default:+1.8:+1.9:+1.10" "test"]}
+                 [crypto-equality "1.0.0"]
+                 [instaparse "1.4.10" :exclusions [org.clojure/clojure]]]
+            :aliases {"test-all" ["with-profile" "default:+1.8:+1.9:+1.10" "test"]}
   :profiles
   {:provided {:dependencies [[javax.servlet/servlet-api "2.5"]]}
    :dev  {:dependencies [[clj-time "0.15.2"]
--- ring-core/src/ring/middleware/range.clj
+++ ring-core/src/ring/middleware/range.clj
@@ -2,101 +2,59 @@
   "Middleware that handles HTTP range requests"
   (:require [clojure.java.io :as io]
             [clojure.string :as str]
-            [clojure.spec.alpha :as s]
+            [instaparse.core :as insta]
             [ring.core.protocols :refer :all]
             [ring.middleware.content-type :refer [content-type-response]]
             [ring.util.response :refer [status get-header header]])
   (:import [java.io ByteArrayInputStream ByteArrayOutputStream File IOException InputStream RandomAccessFile]))
 
-(def has-at-least-one-digit (s/+ #{\0 \1 \2 \3 \4 \5 \6 \7 \8 \9}))
-(def optional-whitespace (s/* #{\space \tab}))
+(def header-value-parser
+  (insta/parser
+    "S=byte-ranges-specifier
+     Accept-Ranges = acceptable-ranges
+     Content-Range = byte-content-range
+     OWS = #\"[ \t]*\"
+     Range = byte-ranges-specifier
+     acceptable-ranges = ( (\",\" OWS)* range-unit (OWS \",\" (OWS range-unit)?)* ) | \"none\"
+     byte-content-range = bytes-unit \" \" ( byte-range-resp | unsatisfied-range )
+     byte-range = first-byte-pos \"-\" last-byte-pos
+     byte-range-resp = byte-range \"/\" (complete-length | \"*\")
+     byte-range-set = (\",\" OWS)* (byte-range-spec | suffix-byte-range-spec) ( OWS \",\" (OWS (byte-range-spec | suffix-byte-range-spec))? )*
+     byte-range-spec = first-byte-pos \"-\" last-byte-pos?
+     byte-ranges-specifier = bytes-unit \"=\" byte-range-set
+     bytes-unit = \"bytes\"
+     complete-length = #\"\\d+\"
+     first-byte-pos = #\"\\d+\"
+     last-byte-pos = #\"\\d+\"
+     range-unit = bytes-unit
+     suffix-byte-range-spec = \"-\" suffix-length
+     suffix-length = #\"\\d+\"
+     unsatisfied-range = \"*/\" complete-length"
+    :output-format :hiccup))
 
-(def range-unit "bytes")
-(s/def ::bytes-unit (s/cat :b #{\b}
-                           :y #{\y}
-                           :t #{\t}
-                           :e #{\e}
-                           :s #{\s}))
-(s/def ::acceptable-ranges #{(seq range-unit) (seq "none")})
-(s/def ::first-byte-pos has-at-least-one-digit)
-(s/def ::last-byte-pos has-at-least-one-digit)
-(s/def ::complete-length has-at-least-one-digit)
-(s/def ::byte-content-range (s/cat :bytes-unit ::bytes-unit
-                                   :space #{\space}
-                                   :range (s/or :byte-range-resp ::byte-range-resp
-                                                :unsatisfied-range ::unsatisfied-range)))
-(s/def ::byte-range (s/cat :first-byte-pos ::first-byte-pos
-                           :dash #{\-}
-                           :last-byte-pos ::last-byte-pos))
-(s/def ::byte-range-resp (s/cat :byte-range ::byte-range
-                                :slash #{\/}
-                                :length (s/or :complete-length ::complete-length
-                                              :star #{\*})))
-(s/def ::byte-range-set (s/cat :pre-ranges (s/* (s/cat :comma #{\,}
-                                                       :optional-whitespace optional-whitespace))
-                               :first-byte-range (s/or :byte-range-spec ::byte-range-spec
-                                                       :suffix-byte-range-spec ::suffix-byte-range-spec)
-                               :rest-of-byte-ranges (s/* (s/cat :optional-whitespace optional-whitespace
-                                                                :comma #{\,}
-                                                                :ranges (s/cat :optional-whitespace optional-whitespace
-                                                                               :spec (s/or :byte-range-spec ::byte-range-spec
-                                                                                           :suffix-byte-range-spec ::suffix-byte-range-spec))))))
-(comment
-  (s/conform ::byte-range-set (seq ",  "))
-  (s/conform (s/cat :a (s/or :a #{\a})))
-  (s/conform (s/cat :top (s/+ #{0 1 2 3 4}))
-             [0 1 2 3])
-  (s/conform (s/cat :foo (s/or :test (s/+ #{\0 \1 \2 \3 \4 \5 \6 \7 \8 \9})
-                          :test2 (s/+ #{\a \b \c \d})))
-             (seq "0123"))
-  (s/conform (s/cat :first-range
-                    (s/or
-                      :test (s/+ #{\0 \1 \2 \3 \4 \5 \6 \7 \8 \9}))
-                    #_(s/or :bytes (s/+ #{\0 \1 \2 \3 \4 \5 \6 \7 \8 \9})
-                                       :suffix #{\b}))
-             [\2 \3])
-  (s/conform ::byte-range-set (seq "34-45,67-89"))
-  (s/conform (s/or :suffix ::suffix-byte-range-spec
-                   :byte-range ::byte-range-spec)
-             (seq "34-56"))
-  (s/conform (s/cat :lol (s/or :a (s/+ #{\1 \2 \3})
-                               :b #{\b}))
-             [\1]
-             )
-  (s/conform (s/cat :a (s/+ (s/cat :b #{\a \b \c})))
-             [\a \b])
-  (s/conform (s/cat :a (s/+ (s/cat :b (s/or :abc #{\a \b \c}
-                                            :num #{\1 \2 \3}))))
-             [\a \b])
-  (s/conform (s/cat :a (s/cat :b (s/+ #{\a \b \c})))
-             [\a \b]
-             #_[[\a] [\b]])
-  (s/conform (s/cat :first-byte-range (s/or
-                                        :byte-range ::byte-range-spec
-                                        :suffix ::suffix-byte-range-spec))
-             (seq "56-"))
-  (s/conform (s/or ::byte-range-spec ::suffix-byte-range-spec) (seq "-56"))
-  (s/conform ::suffix-byte-range-spec (seq "-56"))
-  (s/conform ::byte-range-set (seq "-56"))
-  (s/conform ::byte-range-set (seq "34-56,-5")))
-(s/def ::byte-range-spec (s/cat :first-byte-pos ::first-byte-pos
-                                :dash #{\-}
-                                :last-byte-pos (s/? ::last-byte-pos)))
-(s/def ::byte-ranges-specifier (s/cat :bytes-unit ::bytes-unit
-                                      :equals #{\=}
-                                      :byte-range-set ::byte-range-set))
-(s/def ::suffix-length has-at-least-one-digit)
-(s/def ::suffix-byte-range-spec (s/cat :dash #{\-}
-                                       :digits has-at-least-one-digit))
-(s/def ::unsatisfied-range (s/cat :star #{\*}
-                                  :slash #{\/}
-                                  :length ::complete-length))
+(defn- get-hiccup-children-by-tags
+  [tags hiccup]
+  {:pre [(set? tags)]}
+  (when (vector? hiccup)
+    (let [children (subvec hiccup 1)]
+      (filter #(contains? tags (first %)) children))))
 
-(comment
-  (header-value->ranges "bytes=34-56")
-  (header-value->ranges "bytes=34-56, -5")
-  )
-
+(defn header-value->ranges
+  [^String header-value]
+  (let [output (header-value-parser header-value)]
+    (when-not (insta/failure? output)
+      (->> output
+           (get-hiccup-children-by-tags #{:byte-ranges-specifier})
+           (mapcat #(get-hiccup-children-by-tags #{:byte-range-set} %))
+           (mapcat #(get-hiccup-children-by-tags #{:byte-range-spec :suffix-byte-range-spec} %))
+           (map (fn [range-spec]
+                  (let [tags (case (first range-spec)
+                               :byte-range-spec #{:first-byte-pos :last-byte-pos}
+                               :suffix-byte-range-spec #{:suffix-length})]
+                    (->> (get-hiccup-children-by-tags tags range-spec)
+                         (map (fn [[position-type num]] [position-type (Long/parseLong num)]))
+                         (into {})))))
+           (sort-by (juxt :suffix-length :first-byte-pos :last-byte-pos))))))
 
 (defn- char-range
   [start end]
@@ -109,35 +67,6 @@
                                            (take 30)
                                            (map #(nth boundary-chars %))
                                            (apply str))))
-
-(defn header-value->ranges
-  [^String header-value]
-  (let [result (s/conform ::byte-ranges-specifier (seq header-value))]
-    result)
-  #_(when (.startsWith header-value (str range-unit "="))
-    (let [rest (-> header-value
-                   (subs (count range-unit))
-                   ;; get rid of equal sign
-                   (subs 1))
-          split-groups (str/split rest #",")
-          parsed-groups (map #(re-find #"^\s*(\d*)-(\d*)\s*$" %) split-groups)]
-      (map (fn [[_ prefix suffix]]
-             (let [prefix-int (if (empty? prefix) nil (Long/parseLong prefix))
-                   suffix-int (if (empty? suffix) nil (Long/parseLong suffix))]
-               (cond
-                 (and (some? prefix-int) (some? suffix-int))
-                 {:start prefix-int
-                  :end suffix-int}
-
-                 (some? prefix-int)
-                 {:start prefix-int}
-
-                 (some? suffix-int)
-                 {:start (* -1 suffix-int)}
-
-                 :else
-                 {})))
-           parsed-groups))))
 
 (defn parse-ranges
   "If all byte ranges are valid and not overlapping, returns absolute byte offsets in order from smallest start/end
