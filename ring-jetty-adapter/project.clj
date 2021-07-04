bf6661ec2801c4ed0cde3af0463997bff3c950c1
Juho Leinonen <juholei@gmail.com>
2021-07-04 15:24:54 +0300

    Update Jetty to 9.4.42
    Fixes two known vulnerabilities in Jetty:
    - https://nvd.nist.gov/vuln/detail/CVE-2021-28169
    - https://nvd.nist.gov/vuln/detail/CVE-2021-34428

--- ring-jetty-adapter/project.clj
+++ ring-jetty-adapter/project.clj
@@ -7,7 +7,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [ring/ring-core "1.9.3"]
                  [ring/ring-servlet "1.9.3"]
-                 [org.eclipse.jetty/jetty-server "9.4.40.v20210413"]]
+                 [org.eclipse.jetty/jetty-server "9.4.42.v20210604"]]
   :aliases {"test-all" ["with-profile" "default:+1.8:+1.9:+1.10" "test"]}
   :profiles
   {:dev  {:dependencies [[clj-http "3.10.0"]
