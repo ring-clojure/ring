fd257f36e2d12e199d245f4acf5a8987a9918abf
Nick Mertin <nickmertin@gmail.com>
2021-07-24 13:40:26 -0400

    Add unit tests for byte-array-store with :max-size

--- ring-core/test/ring/middleware/multipart_params/test/byte_array.clj
+++ ring-core/test/ring/middleware/multipart_params/test/byte_array.clj
@@ -12,3 +12,24 @@
     (is (= (:filename result) "foo.txt"))
     (is (= (:content-type result) "text/plain"))
     (is (= (String. (:bytes result)) "foo"))))
+
+(deftest test-byte-array-store-max-size-success
+  (let [store  (byte-array-store {:max-size 100})
+        result (store
+                {:filename "foo.txt"
+                 :content-type "text/plain"
+                 :stream (string-input-stream "foo")})]
+    (is (= (:filename result) "foo.txt"))
+    (is (= (:content-type result) "text/plain"))
+    (is (= (String. (:bytes result)) "foo"))))
+
+(deftest test-byte-array-store-max-size-failure
+  (let [store  (byte-array-store {:max-size 1})
+        result (store
+                {:filename "foo.txt"
+                 :content-type "text/plain"
+                 :stream (string-input-stream "foo")})]
+    (is (= (:filename result) "foo.txt"))
+    (is (= (:content-type result) "text/plain"))
+    (is (= (:bytes result)
+           :ring.middleware.multipart-params.byte-array/too-large))))
