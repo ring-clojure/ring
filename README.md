c998c5a698de28ad80c89c21989347ae42b76ac2
James Reeves <jreeves@weavejester.com>
2022-01-15 14:26:16 +0000

    Release 1.9.5

--- README.md
+++ README.md
@@ -39,11 +39,11 @@ To resolve this, include the following dependency in your dev profile:
 To include one of the above libraries, for example `ring-core`, add
 the following to your `:dependencies`:
 
-    [ring/ring-core "1.9.4"]
+    [ring/ring-core "1.9.5"]
 
 To include all of them:
 
-    [ring "1.9.4"]
+    [ring "1.9.5"]
 
 ## Documentation
 
--- ring-bench/project.clj
+++ ring-bench/project.clj
@@ -1,4 +1,4 @@
-(defproject ring/ring-bench "1.9.4"
+(defproject ring/ring-bench "1.9.5"
   :description "Ring core libraries."
   :url "https://github.com/ring-clojure/ring"
   :scm {:dir ".."}
@@ -6,7 +6,7 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [jmh-clojure "0.4.0"]
-                 [ring/ring-jetty-adapter "1.9.4"]
-                 [ring/ring-servlet "1.9.4"]]
+                 [ring/ring-jetty-adapter "1.9.5"]
+                 [ring/ring-servlet "1.9.5"]]
   :jvm-opts {}
   :main ring.bench.servlet)
