18513e205ca2348cd5fcf30c50c3a3b4f36f58dd
Michael Easter <measter@splashfinancial.com>
2022-07-08 11:04:27 -0500

    Update jetty-server to 9.4.48.v20220622

--- ring-jetty-adapter/project.clj
+++ ring-jetty-adapter/project.clj
@@ -7,7 +7,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [ring/ring-core "1.9.5"]
                  [ring/ring-servlet "1.9.5"]
-                 [org.eclipse.jetty/jetty-server "9.4.44.v20210927"]]
+                 [org.eclipse.jetty/jetty-server "9.4.48.v20220622"]]
   :aliases {"test-all" ["with-profile" "default:+1.8:+1.9:+1.10" "test"]}
   :profiles
   {:dev  {:dependencies [[clj-http "3.12.3"]
