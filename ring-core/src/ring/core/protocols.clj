736c5e6078a6f0f39453709d9b2a6e7c8ad6be9d
James Reeves <jreeves@weavejester.com>
2023-08-14 15:20:31 +0100

    Clarify write-body-to-stream docstring
    Explicitly state that the output stream may be written to
    asynchronously in the write-body-to-stream docstring. This allows for
    asynchronous channels or streams to be returned as a response body.
    Previously it may not have been obvious that this was possible
    behavior.

--- ring-core/src/ring/core/protocols.clj
+++ ring-core/src/ring/core/protocols.clj
@@ -9,7 +9,8 @@
   "A protocol for writing data to the response body via an output stream."
   (write-body-to-stream [body response output-stream]
     "Write a value representing a response body to an output stream. The stream
-    will be closed after the value had been written."))
+    will be closed after the value had been written. The stream may be written
+    asynchronously."))
 
 (defn- ^Writer response-writer [response output-stream]
   (if-let [charset (response/get-charset response)]
