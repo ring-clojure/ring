3838af6da5d8d2778494c64e9283709c93f5074e
James Reeves <jreeves@weavejester.com>
2023-10-12 23:24:55 +0100

    Move map listener test from Jetty to core

--- ring-core/test/ring/test/websocket.clj
+++ ring-core/test/ring/test/websocket.clj
@@ -1,6 +1,7 @@
 (ns ring.test.websocket
-  (:require [clojure.test :refer [deftest is testing]]
-            [ring.websocket :as ws]))
+  (:require [clojure.test :refer [deftest is]]
+            [ring.websocket :as ws]
+            [ring.websocket.protocols :as wsp]))
 
 (deftest test-request-protocols
   (is (empty? (ws/request-protocols {:headers {}})))
@@ -9,3 +10,20 @@
   (is (= ["mqtt" "soap"]
          (ws/request-protocols
           {:headers {"sec-websocket-protocol" "mqtt, soap"}}))))
+
+(deftest test-map-listeners
+  (let [listener {:on-open (fn [s] [:on-open s])
+                  :on-message (fn [s m] [:on-message s m])
+                  :on-pong (fn [s d] [:on-pong s d])
+                  :on-error (fn [s e] [:on-error s e])
+                  :on-close (fn [s c r] [:on-close s c r])}]
+    (is (= [:on-open :sock]
+           (wsp/on-open listener :sock)))
+    (is (= [:on-message :sock "foo"]
+           (wsp/on-message listener :sock "foo")))
+    (is (= [:on-pong :sock "data"]
+           (wsp/on-pong listener :sock "data")))
+    (is (= [:on-error :sock "err"]
+           (wsp/on-error listener :sock "err")))
+    (is (= [:on-close :sock 1000 "closed"]
+           (wsp/on-close listener :sock 1000 "closed")))))
--- ring-jetty-adapter/test/ring/adapter/test/jetty.clj
+++ ring-jetty-adapter/test/ring/adapter/test/jetty.clj
@@ -824,21 +824,4 @@
           (Thread/sleep 100)
           @(hato/close! ws)
           (Thread/sleep 100)))
-      (is (= ["Hello" "World"] @log))))
-
-  (testing "using a map as a listener"
-    (let [listener {:on-open (fn [s] [:on-open s])
-                    :on-message (fn [s m] [:on-message s m])
-                    :on-pong (fn [s d] [:on-pong s d])
-                    :on-error (fn [s e] [:on-error s e])
-                    :on-close (fn [s c r] [:on-close s c r])}]
-      (is (= [:on-open :sock]
-             (wsp/on-open listener :sock)))
-      (is (= [:on-message :sock "foo"]
-             (wsp/on-message listener :sock "foo")))
-      (is (= [:on-pong :sock "data"]
-             (wsp/on-pong listener :sock "data")))
-      (is (= [:on-error :sock "err"]
-             (wsp/on-error listener :sock "err")))
-      (is (= [:on-close :sock 1000 "closed"]
-             (wsp/on-close listener :sock 1000 "closed"))))))
+      (is (= ["Hello" "World"] @log)))))
