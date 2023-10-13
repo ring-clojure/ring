3084e24885f1975d0ab28d5ff9c610b7b39ec2c7
James Reeves <jreeves@weavejester.com>
2023-10-13 02:35:00 +0100

    Update Jetty dependency to 11.0.17

--- ring-jetty-adapter/project.clj
+++ ring-jetty-adapter/project.clj
@@ -7,8 +7,8 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [ring/ring-core "1.11.0-alpha4"]
                  [org.ring-clojure/ring-jakarta-servlet "1.11.0-alpha4"]
-                 [org.eclipse.jetty/jetty-server "11.0.16"]
-                 [org.eclipse.jetty.websocket/websocket-jetty-server "11.0.16"]]
+                 [org.eclipse.jetty/jetty-server "11.0.17"]
+                 [org.eclipse.jetty.websocket/websocket-jetty-server "11.0.17"]]
   :aliases {"test-all" ["with-profile" "default:+1.8:+1.9:+1.10:+1.11" "test"]}
   :profiles
   {:dev  {:dependencies [[clj-http "3.12.3"]
