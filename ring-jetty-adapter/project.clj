b5ec0e70860efd2f114c300a8f7ef04a07cbe5a9
James Reeves <jreeves@weavejester.com>
2024-02-03 19:15:44 +0000

    Update dependencies

--- ring-jetty-adapter/project.clj
+++ ring-jetty-adapter/project.clj
@@ -7,14 +7,14 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [ring/ring-core "1.11.0"]
                  [org.ring-clojure/ring-jakarta-servlet "1.11.0"]
-                 [org.eclipse.jetty/jetty-server "11.0.18"]
-                 [org.eclipse.jetty.websocket/websocket-jetty-server "11.0.18"]]
+                 [org.eclipse.jetty/jetty-server "11.0.20"]
+                 [org.eclipse.jetty.websocket/websocket-jetty-server "11.0.20"]]
   :aliases {"test-all" ["with-profile" "default:+1.8:+1.9:+1.10:+1.11" "test"]}
   :profiles
   {:dev  {:dependencies [[clj-http "3.12.3"]
                          [less-awful-ssl "1.0.6"]
                          [hato "0.9.0"]
-                         [org.slf4j/slf4j-simple "2.0.9"]]
+                         [org.slf4j/slf4j-simple "2.0.11"]]
           :jvm-opts ["-Dorg.eclipse.jetty.server.HttpChannelState.DEFAULT_TIMEOUT=500"]}
    :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
    :1.9  {:dependencies [[org.clojure/clojure "1.9.0"]]}
